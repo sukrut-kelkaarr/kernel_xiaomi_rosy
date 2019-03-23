1ea9b71d2456ee72fe7513966ef091fd04cd9b23
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-03-23 23:47:13 +0530

    fixed derp

--- drivers/char/diag/diagfwd.c
+++ drivers/char/diag/diagfwd.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 /* Copyright (c) 2008-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2018 XiaoMi, Inc.
-=======
-/* Copyright (c) 2008-2019, The Linux Foundation. All rights reserved.
->>>>>>> 7c90459655d4c758cb77cb177743df94a5f08963
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
